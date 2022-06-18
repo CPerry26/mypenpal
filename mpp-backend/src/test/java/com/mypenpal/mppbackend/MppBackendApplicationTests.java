447c14c21fa96fd77f0e123c9bcd206be94ae74d
CPerry26 <codyy@live.com>
2022-06-18 10:27:21 -0400

    Backend project created.

---
+++ mpp-backend/src/test/java/com/mypenpal/mppbackend/MppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypenpal.mppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
